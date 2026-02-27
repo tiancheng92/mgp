7a9654686476793b168ff5e7dbb1770ac56b80d3
tiancheng.xu <kiritoeva@icloud.com>
2026-02-27 18:02:50 +0800

    Add new return type utility functions and import `net/http`

--- return_type.go
+++ return_type.go
@@ -1,6 +1,43 @@
 package mgp
 
+import "net/http"
+
 type ReturnType struct {
 	StatusCode int
 	Body       any
+}
+
+func RT[T any](statusCode ...int) *ReturnType {
+	hc := http.StatusOK
+	if len(statusCode) != 0 {
+		hc = statusCode[0]
+	}
+
+	return &ReturnType{
+		StatusCode: hc,
+		Body:       new(Result[T]),
+	}
+}
+
+func RTE(statusCode ...int) *ReturnType {
+	hc := http.StatusOK
+	if len(statusCode) != 0 {
+		hc = statusCode[0]
+	}
+
+	return &ReturnType{
+		StatusCode: hc,
+	}
+}
+
+func PRT[T any](statusCode ...int) *ReturnType {
+	hc := http.StatusOK
+	if len(statusCode) != 0 {
+		hc = statusCode[0]
+	}
+
+	return &ReturnType{
+		StatusCode: hc,
+		Body:       new(Result[PaginateData[[]T]]),
+	}
 }
